db60596468ee4a05493b5a459d0984f0ce65b59a
Th3GEM <amsamkaranja@gmail.com>
2025-11-12 20:38:56 +0300

    run npm test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Plantsy Admin header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/plantsy admin/i);
+  expect(headerElement).toBeInTheDocument();
 });
